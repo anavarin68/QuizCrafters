b74c5a070e3fa20d4d63794a1f09f050852cae56
nkp <kumarpateln7@gmail.com>
2024-01-16 22:27:19 +0530

    Initial Files

---
+++ script.js
@@ -0,0 +1,8 @@
+// document.getElementById('signUp').addEventListener('click', function () {
+//     document.getElementById('container').classList.add('right-panel-active');
+//   });
+  
+//   document.getElementById('signIn').addEventListener('click', function () {
+//     document.getElementById('container').classList.remove('right-panel-active');
+//   });
+  
