49ea17993f869989046a87e636c4ccc2fec411ac
nkp <kumarpateln7@gmail.com>
2024-01-17 01:43:14 +0530

    added options

--- Quiz/js/questions.js
+++ Quiz/js/questions.js
@@ -55,9 +55,7 @@ let questions = [
       "eXamine Multiple Language"
     ]
   },
-  // you can uncomment the below codes and make duplicate as more as you want to add question
-  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
-
+  
   //   {
   //   numb: 6,
   //   question: "Your Question is Here",
